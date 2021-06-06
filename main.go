f79548cd94e3156442404669daedd3d9536c5296
Weerasak Chongnguluam <singpor@gmail.com>
2021-06-06 16:06:42 +0700

    Demo Go long running backgroud service

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type worker struct {
+	name     string
+	duration time.Duration
+	done     chan struct{}
+}
+
+func sleepContext(ctx context.Context, duration time.Duration) {
+	select {
+	case <-ctx.Done():
+		return
+	case <-time.After(duration):
+		return
+	}
+}
+
+func (w *worker) Execute(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("%s: DONE", w.name)
+			close(w.done)
+			return
+		default:
+			log.Printf("%s: at %s", w.name, time.Now().Format(time.RFC3339))
+			sleepContext(ctx, w.duration)
+		}
+	}
+}
+
+func (w *worker) Wait() {
+	<-w.done
+}
+
+func NewWorker(name string, d time.Duration) *worker {
+	return &worker{
+		done:     make(chan struct{}),
+		name:     name,
+		duration: d,
+	}
+}
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	w := NewWorker("worker 1", 1*time.Second)
+	go w.Execute(ctx)
+
+	w2 := NewWorker("worker 2", 3*time.Second)
+	go w2.Execute(ctx)
+
+	w.Wait()
+	w2.Wait()
+}
